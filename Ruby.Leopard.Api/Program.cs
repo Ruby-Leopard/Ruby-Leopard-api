1bbb0b3bd5004cb0886af0f1f57991c7a17e9189
Jingxuan Hu <Hu.1789@buckeyemail.osu.edu>
2022-04-25 19:49:25 -0400

    Securing API

--- Ruby.Leopard.Api/Program.cs
+++ Ruby.Leopard.Api/Program.cs
@@ -1,12 +1,37 @@
 using Ruby.Leopard.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Sqlite;
+using Ruby.Leopard.Api.Sequrity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+string authority = builder.Configuration["Auth0:Authority"] ??
+    throw new ArgumentException("Auth0:Authority");
+
+string audience = builder.Configuration["Auth0:Audience"] ??
+    throw new ArgumentNullException("Auth0:Audience");    
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+    builder.Services.AddAuthentication(options =>
+{
+    options.AddPolicy("delete:catalog",policy =>
+        policy.RequireAuthenticatedUser().RequireClaim("scope", "delete:catalog"));    
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+
+.AddJwtBearer(options =>
+{
+    options.Authority = authority;
+    options.Audience = audience;
+});
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlite("Data Source=../Registrar.sqlite",
@@ -36,6 +61,10 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.UseAuthorization();
 
