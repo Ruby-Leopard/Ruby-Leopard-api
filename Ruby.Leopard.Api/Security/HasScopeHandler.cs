05c94be8456d2d0a74b6a25a5036a4fa8c9ebd1d
Jingxuan Hu <Hu.1789@buckeyemail.osu.edu>
2022-04-25 19:19:35 -0400

    security

---
+++ Ruby.Leopard.Api/Security/HasScopeHandler.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Ruby.Leopard.Api.Sequirty
+{
+    public class HasScopeHandler : AuthorizationHandler<HasScopeRequirements>
+    {
+        protected override Task HansleRequirementAsync(
+            AuthorizationHandlerContext context,
+            HasScopeRequirements requirements)
+        {
+
+            if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == requirements.Issuer))
+                return Task.CompletedTask;
+
+            var scopes = context.User
+                .FindFirst(c => c.Type == "scope" && c.Issuer == requirements.Issuer)    
+                .Value.Split(' ');
+
+            if (scopes.Any(s => s == requirement.Scope))
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;        
+        }
+    }
+}
