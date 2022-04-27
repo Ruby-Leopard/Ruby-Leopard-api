3246ab468d9205a4db040d7ef9816f063d5c92b2
Jingxuan Hu <Hu.1789@buckeyemail.osu.edu>
2022-04-27 18:31:39 -0400

    rename

--- Ruby.Leopard.Api/Security/HasScopeHandler.cs
+++ Ruby.Leopard.Api/Security/HasScopeHandler.cs
@@ -11,11 +11,11 @@ namespace Ruby.Leopard.Api.Sequirty
             HasScopeRequirements requirement)
         {
 
-            if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == requirements.Issuer))
+            if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == requirement.Issuer))
                 return Task.CompletedTask;
 
             var scopes = context.User
-                .FindFirst(c => c.Type == "scope" && c.Issuer == requirements.Issuer)    
+                .FindFirst(c => c.Type == "scope" && c.Issuer == requirement.Issuer)    
                 .Value.Split(' ');
 
             if (scopes.Any(s => s == requirement.Scope))
