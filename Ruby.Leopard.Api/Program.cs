656277e0f242117cf4708747a91936e8251f8c68
Vybhav Vydula <vydula.2@osu.edu>
2022-05-02 14:05:10 -0400

    new

--- Ruby.Leopard.Api/Program.cs
+++ Ruby.Leopard.Api/Program.cs
@@ -13,6 +13,9 @@ string authority = builder.Configuration["Auth0:Authority"] ??
 string audience = builder.Configuration["Auth0:Audience"] ??
     throw new ArgumentNullException("Auth0:Audience");    
 
+string storeConnectionString = builder.Configuration.GetConnectionString("StoreConnection") ??
+    throw new ArgumentNullException("ConnectionString:StoreConnection");
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -39,7 +42,7 @@ builder.Services.AddControllers();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddDbContext<StoreContext>(options =>
-    options.UseSqlite("Data Source=../Registrar.sqlite",
+    options.UseSqlServer(storeConnectionString,
     b => b.MigrationsAssembly("Ruby.Leopard.Api"))
 );
 builder.Services.AddCors(Options =>
